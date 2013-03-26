8346dc1e0c5848c6a3b8ab2bb7e6d9ca057da097
Nilam Doctor <nilamdoc@gmail.com>
2013-03-27 00:57:44 +0530

    Initial

---
+++ app/resources/tmp/cache/templates/template_views_elements_footer.html_0_1364323906_409.php
@@ -0,0 +1,10 @@
+<div id="footer" style="padding:1px 20px; border-top:1px solid black" class="navbar-inner navbar ">
+	<ul class="nav" style="font-size:11px ">
+		<li><a>&copy; bitcoinverified.com</a></li>
+		<li><a href="#">Contact us</a></li>		
+		<li><a href="#">Credit Links</a></li>		
+		<li><a href="#">Better credits</a></li>		
+		<li><a href="#">Privacy</a></li>		
+		<li><a href="#">Legal</a></li>		
+	</ul>
+</div>
